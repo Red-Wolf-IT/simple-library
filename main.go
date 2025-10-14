b7b142247e1283e8b71da8677001d0766674377f
Efim <jacober@list.ru>
2025-10-14 14:59:33 +0300

    Add Go application with multiple files

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
+import "fmt"
+
 func main() {
-	user1 = Reader{
+	user1 := Reader{
 		ID: 1,
 		FirstName: "Ivan",
 		LastName: "Ivanovich",
 		IsActive: true,
 	}
 
-	user1.Deactivate()
-	user1.DisplayReader()
+	book1 := Book{
+		ID:       1,
+		Year:     1867,
+		Title:    "Война и мир",
+		Author:   "Лев Толстой",
+		IsIssue: false,
+	}
+
+	fmt.Println(user1)
+	fmt.Println(book1)
+	book1.IssueBook(&user1)
+	fmt.Println(book1)
+	book1.ReturnBook()
+	fmt.Println(book1)
+	user1.AssignBook(&book1)
 }
--- models.go
+++ models.go
@@ -9,8 +9,8 @@ type Reader struct {
 	IsActive	bool
 }
 
-func (r Rider) DisplayReader() {
-	fmt.Printf("Читатель: %s %s (ID: %d)\n", r.FirstName, r.LastName, r.ID)
+func (r Reader) DisplayReader() {
+	fmt.Printf("Читатель: %s %s (ID: %d)(Status: %v)\n", r.FirstName, r.LastName, r.ID, r.IsActive)
 }
 
 func (r *Reader) Deactivate() {
@@ -28,8 +28,46 @@ func (r Reader) String() string {
 }
 
 type Book struct {
-	ID		int
-	Title 	string
-	Author 	string
-	IsIssue bool
+	ID			int
+	Title 		string
+	Author 		string
+	Year		int
+	IsIssue 	bool
+	ReaderID 	*int
+}
+
+func (b Book) String() string{
+	status := ""
+	if b.IsIssue {
+		status = "используется."
+		return fmt.Sprintf("ID: %d, %s (%s, %d), книга %s читателем %d", b.ID, b.Title, b.Author, b.Year, status, *b.ReaderID)
+	} else {
+		status = "не используется."
+		return fmt.Sprintf("ID: %d, %s (%s, %d), книга %s", b.ID, b.Title, b.Author, b.Year, status)
+	}
+	
+}
+
+func (b *Book) IssueBook(r *Reader) {
+	if b.IsIssue{
+		fmt.Println("Книга уже используется.")
+	} else {
+		b.IsIssue = true
+		b.ReaderID = &r.ID
+		fmt.Printf("Книга выдана читателю %s %s.\n", r.FirstName, r.LastName)
+	}
+}
+
+func (b *Book) ReturnBook() {
+	if !b.IsIssue{
+		fmt.Println("Книга уже в библиотеке.")
+	} else {
+		b.IsIssue = false
+		b.ReaderID = nil
+		fmt.Println("Книга возвращена.")
+	}
+}
+
+func (r *Reader) AssignBook(b *Book) {
+	fmt.Printf("Читатель %s %s взял книгу %s(%s %d)", r.FirstName, r.LastName, b.Title, b.Author, b.Year)
 }
