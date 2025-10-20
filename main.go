857eaa21dd541c5a7471e631940646e849eab4e8
Efim <jacober@list.ru>
2025-10-20 14:39:25 +0300

    добавлене методов: IssueBookToReader, FindBookByID, IssueBookToReader и т.д.

--- main.go
+++ main.go
@@ -3,32 +3,66 @@ package main
 import "fmt"
 
 func main() {
-	user1 := Reader{
-		ID: 1,
-		FirstName: "Ivan",
-		LastName: "Ivanovich",
-		IsActive: true,
+	// user1 := Reader{
+	// 	ID: 1,
+	// 	FirstName: "Ivan",
+	// 	LastName: "Ivanovich",
+	// 	IsActive: true,
+	// }
+
+	// book1 := Book{
+	// 	ID:       1,
+	// 	Year:     1867,
+	// 	Title:    "Война и мир",
+	// 	Author:   "Лев Толстой",
+	// 	IsIssue: false,
+	// }
+
+	// fmt.Println(user1)
+	// fmt.Println(book1)
+	// book1.IssueBook(&user1)
+	// fmt.Println(book1)
+	// book1.ReturnBook()
+	// fmt.Println(book1)
+	// user1.AssignBook(&book1)
+
+	// email := EmailNotifier{Email: "test@mail.ru"}
+	// sms := SMSNotifier{PhoneNumber: "89289289898"}
+
+	// email.Notify("test")
+	// sms.Notify("test")
+
+
+	fmt.Println("Загрузка системы управления библиотекой...")
+
+	myLibrary := &Library{}
+
+	fmt.Println("\n--- Заполнение библиотеки ---")
+
+	myLibrary.AddReader("Иван", "Иванович")
+	myLibrary.AddReader("Антон", "Сергеевич")
+
+	myLibrary.AddBook("1984", "Джордж Оруэлл", 1949)
+	myLibrary.AddBook("Мастер и Маргарита", "Михаил Булгаков", 1967)
+
+	fmt.Println("\n--- Библиотека готова к работе ---")
+	fmt.Println("Количество читателей:", len(myLibrary.Readers))
+	fmt.Println("Количество книг:", len(myLibrary.Books))
+
+	fmt.Println("--- Тестируем выдачу книг ---")
+
+	err := myLibrary.IssueBookToReader(1, 1)
+	if err != nil {
+		fmt.Println("Ошибка выдачи", err)
 	}
 
-	book1 := Book{
-		ID:       1,
-		Year:     1867,
-		Title:    "Война и мир",
-		Author:   "Лев Толстой",
-		IsIssue: false,
+	book, _ := myLibrary.FindBookByID(1)
+	if book != nil {
+		fmt.Println("Статус книги после выдачи:", book)
 	}
 
-	fmt.Println(user1)
-	fmt.Println(book1)
-	book1.IssueBook(&user1)
-	fmt.Println(book1)
-	book1.ReturnBook()
-	fmt.Println(book1)
-	user1.AssignBook(&book1)
-
-	email := EmailNotifier{Email: "test@mail.ru"}
-	sms := SMSNotifier{PhoneNumber: "89289289898"}
-
-	email.Notify("test")
-	sms.Notify("test")
+	err = myLibrary.IssueBookToReader(12, 1)
+	if err != nil {
+		fmt.Println("Ожидаемая ошибка:", err)
+	}
 }
--- models.go
+++ models.go
@@ -2,6 +2,48 @@ package main
 
 import "fmt"
 
+type Library struct {
+	Books	[]*Book
+	Readers	[]*Reader
+
+	lastBookID		int
+	lastReaderID	int
+}
+
+func (lib *Library) AddReader(firstName, lastName string) *Reader {
+	lib.lastReaderID++
+
+	newReader := &Reader{
+		ID:			lib.lastReaderID,
+		FirstName:	firstName,
+		LastName:	lastName,
+		IsActive:	true,
+	}
+
+	lib.Readers = append(lib.Readers, newReader)
+
+	fmt.Printf("Добавлен новый читатель: %s\n", newReader)
+	return newReader
+}
+
+func (lib *Library) AddBook(title, author string, year int) *Book {
+	lib.lastBookID++
+
+	newBook := &Book{
+		ID:       lib.lastBookID,
+		Title:    title,
+		Author:   author,
+		Year:     year,
+		IsIssued:  false,
+		ReaderID: new(int),
+	}
+
+	lib.Books = append(lib.Books, newBook)
+	fmt.Printf("Добавлена новая книга: %s\n", newBook)
+	return newBook
+}
+
+
 type Reader struct {
 	ID			int
 	FirstName	string
@@ -32,14 +74,14 @@ type Book struct {
 	Title 		string
 	Author 		string
 	Year		int
-	IsIssue 	bool
+	IsIssued 	bool
 	ReaderID 	*int
 }
 
 func (b Book) String() string{
 	status := ""
-	if b.IsIssue {
-		status = "используется."
+	if b.IsIssued {
+		status = "используется"
 		return fmt.Sprintf("ID: %d, %s (%s, %d), книга %s читателем %d", b.ID, b.Title, b.Author, b.Year, status, *b.ReaderID)
 	} else {
 		status = "не используется."
@@ -49,20 +91,20 @@ func (b Book) String() string{
 }
 
 func (b *Book) IssueBook(r *Reader) {
-	if b.IsIssue{
+	if b.IsIssued{
 		fmt.Println("Книга уже используется.")
 	} else {
-		b.IsIssue = true
+		b.IsIssued = true
 		b.ReaderID = &r.ID
 		fmt.Printf("Книга выдана читателю %s %s.\n", r.FirstName, r.LastName)
 	}
 }
 
 func (b *Book) ReturnBook() {
-	if !b.IsIssue{
+	if !b.IsIssued{
 		fmt.Println("Книга уже в библиотеке.")
 	} else {
-		b.IsIssue = false
+		b.IsIssued = false
 		b.ReaderID = nil
 		fmt.Println("Книга возвращена.")
 	}
@@ -70,4 +112,38 @@ func (b *Book) ReturnBook() {
 
 func (r *Reader) AssignBook(b *Book) {
 	fmt.Printf("Читатель %s %s взял книгу %s(%s %d)\n", r.FirstName, r.LastName, b.Title, b.Author, b.Year)
+}
+
+
+func (lib *Library) FindBookByID(id int) (*Book, error) {
+	for i := range lib.Books {
+		if lib.Books[i].ID == id {
+			return lib.Books[i], nil
+		}
+	}
+	return nil, fmt.Errorf("книга с ID %d не найдена", id)
+}
+
+func (lib *Library) FindReaderByID(id int) (*Reader, error) {
+	for i := range lib.Readers {
+		if lib.Readers[i].ID == id {
+			return lib.Readers[i], nil
+		}
+	}
+	return nil, fmt.Errorf("читатель с ID %d не найден", id)
+}
+
+func (lib *Library) IssueBookToReader(bookID, readerID int) error {
+    book, err := lib.FindBookByID(bookID)
+    if err != nil {
+        return err
+    }
+
+    reader, err := lib.FindReaderByID(readerID)
+    if err != nil {
+        return err
+    }
+
+    book.IssueBook(reader)
+    return nil
 }
